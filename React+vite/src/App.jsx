f6d3c62eb776e95b8b5c90f0b3bedd0eebe56aeb
Prajwal <prajwalmp31@gmail.com>
2023-10-05 17:39:43 +0530

    Add some more files and prepared navigation in app.jsx to other files

--- React+vite/src/App.jsx
+++ React+vite/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Begin from "./Begin";
+import Navbar from './Pages/Navbar';
+import { createBrowserRouter,RouterProvider,Router,Link } from 'react-router-dom';
+import First from './Pages/First';
+import Second from './Pages/Second';
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<First/>,
+  },
+  {
+  path:"/second",
+  element:<Second/>
+  }
+])
 function App() {
-
-
-  return (
-  <Begin/>
-
-  )
+return (
+ <div>
+<RouterProvider router={router}/>
+</div>
+  );
 }
 
-export default App
+export default App;
