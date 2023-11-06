469bb8092dc9bbc5d25ca59ed6136323b5c2e7e2
Prajwal Patil <p@m.com>
2023-11-06 12:12:21 +0530

    [Title]:Adding empty, new blog application project. [Description]: Creating multiple rest api's for client application in spring boot tool.

---
+++ rest_blogapplication/src/main/java/com/blog/rest_blogapplication/RestBlogapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.blog.rest_blogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBlogapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBlogapplicationApplication.class, args);
+	}
+
+}
