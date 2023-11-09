c2fdfdb2ef14c87428a2916a698874b5b05e7ff5
Prajwal Patil <p@m.com>
2023-11-09 18:30:08 +0530

    catagory update method added

--- rest_blogapplication/src/main/java/com/blog/rest_blogapplication/Controller/CatagoryController.java
+++ rest_blogapplication/src/main/java/com/blog/rest_blogapplication/Controller/CatagoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public ResponseEntity<CatagoryDto> createCatagory(@RequestBody CatagoryDto catag
 try
 {
 catagoryDto=catagoryService.createCatagory(catagoryDto);
-System.out.println(catagoryDto);
+System.out.println( "New catagory created"+" "+catagoryDto);
 return new ResponseEntity<>(catagoryDto,HttpStatus.CREATED);
 }
 catch(Exception e)
@@ -41,6 +42,7 @@ public ResponseEntity<List<CatagoryDto>> getAll()
     try
     {
     List<CatagoryDto> getall= catagoryService.getAllCatagory();
+    System.out.println("Got all catagory"+" "+getall);
     return new ResponseEntity<List<CatagoryDto>>(getall,HttpStatus.FOUND);
     }
     catch(Exception e)
@@ -49,5 +51,43 @@ public ResponseEntity<List<CatagoryDto>> getAll()
     }
 }
 
+@RequestMapping(path = "/catagory/{id}",method = RequestMethod.GET)
+public ResponseEntity<CatagoryDto> getbyid(@PathVariable (value = "id") int id)
+{
+    try
+    {
+        CatagoryDto getId= catagoryService.getCatagoryById(id);
+        System.out.println("Found catagory by id"+" "+getId);
+        return new ResponseEntity<CatagoryDto>(getId,HttpStatus.OK);
+    }
+    catch (Exception e)
+    {
+        return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+}
+
+@RequestMapping(path = "/catagory/{id}",method = RequestMethod.PUT)
+public ResponseEntity<CatagoryDto> update(@PathVariable (value = "id") int id ,@RequestBody CatagoryDto catagoryDto)
+{
+    try
+    {
+    CatagoryDto existingcatagory=catagoryService.getCatagoryById(id);
+    if(catagoryDto.getDescription() !=null && !catagoryDto.getDescription().isEmpty())
+    {
+     existingcatagory.setDescription(catagoryDto.getDescription());
+    }
+    if(catagoryDto.getTitle() !=null && !catagoryDto.getTitle().isEmpty())
+    {
+        existingcatagory.setTitle(catagoryDto.getTitle());
+    }
+    CatagoryDto updatedCatagory =catagoryService.updateCatagory(existingcatagory);
+    return new ResponseEntity<CatagoryDto>(updatedCatagory,HttpStatus.OK);
+    }
+    catch(Exception e)
+    {
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    
+}
 
 }
