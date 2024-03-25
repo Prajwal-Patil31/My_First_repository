f06d70b65ec33d7dd7b03c5cb0ca78450e94557f
Prajwal_Patil <prajwalmp31@gmail.com>
2024-03-25 16:12:50 +0530

    Initial commit Adding  NOA_Practice folder

---
+++ noa_practice/src/main/java/com/noa/noa_practice/NoaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.noa.noa_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaPracticeApplication.class, args);
+	}
+
+}
