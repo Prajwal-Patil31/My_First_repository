e54f14f41c6c0f9f72a6b5049ec05c009b6699ef
Prajwal_Patil <prajwalmp31@gmail.com>
2024-03-26 14:15:39 +0530

    Modified the noa_practice application

--- noa_practice/src/main/java/com/noa/noa_practice/NoaPracticeApplication.java
+++ noa_practice/src/main/java/com/noa/noa_practice/NoaPracticeApplication.java
@@ -8,6 +8,7 @@ public class NoaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoaPracticeApplication.class, args);
+		System.out.println("Hello java");
 	}
 
 }
